e437397c59776040bb9308350a3d1353145692b1
kshshkhim <kshshkim@gmail.com>
2021-10-24 16:08:29 +0900

    add: board.py

---
+++ client/components/board.py
@@ -0,0 +1,11 @@
+import copy
+
+from src.variables.ui_variables import UI_VARIABLES as uv
+
+matrix = [[0 for y in range(uv.height + 1)] for x in range(uv.width)]  # Board matrix
+
+
+class Board:
+    def __init__(self):
+        self.temp_matrix = copy.deepcopy(matrix)  # 화면 출력용
+        self.frozen_matrix = copy.deepcopy(matrix)  # 충돌 계산용
