3d212805bef86a23fcbb02a3bf99f9f327cb17ca
kshshkhim <kshshkim@gmail.com>
2021-10-24 16:11:21 +0900

    add: ui_variables.py

---
+++ client/src/variables/ui_variables.py
@@ -0,0 +1,53 @@
+import pygame
+from pygame.mixer import Sound
+
+
+class UI_VARIABLES:
+    block_size = 17  # Height, width of single block
+    width = 10  # Board width
+    height = 20  # Board height
+
+    pygame.init()
+    # Fonts
+    font_path = "./assets/fonts/OpenSans-Light.ttf"
+    font_path_b = "./assets/fonts/OpenSans-Bold.ttf"
+    font_path_i = "./assets/fonts/Inconsolata/Inconsolata.otf"
+
+    h1 = pygame.font.Font(font_path, 50)
+    h2 = pygame.font.Font(font_path, 30)
+    h4 = pygame.font.Font(font_path, 20)
+    h5 = pygame.font.Font(font_path, 13)
+    h6 = pygame.font.Font(font_path, 10)
+
+    h1_b = pygame.font.Font(font_path_b, 50)
+    h2_b = pygame.font.Font(font_path_b, 30)
+
+    h2_i = pygame.font.Font(font_path_i, 30)
+    h5_i = pygame.font.Font(font_path_i, 13)
+
+    # Sounds
+    click_sound = Sound("./assets/sounds/SFX_ButtonUp.wav")
+    move_sound = Sound("./assets/sounds/SFX_PieceMoveLR.wav")
+    drop_sound = Sound("./assets/sounds/SFX_PieceHardDrop.wav")
+    single_sound = Sound("./assets/sounds/SFX_SpecialLineClearSingle.wav")
+    double_sound = Sound("./assets/sounds/SFX_SpecialLineClearDouble.wav")
+    triple_sound = Sound("./assets/sounds/SFX_SpecialLineClearTriple.wav")
+    tetris_sound = Sound("./assets/sounds/SFX_SpecialTetris.wav")
+
+    # Background colors
+    black = (10, 10, 10) #rgb(10, 10, 10)
+    white = (255, 255, 255) #rgb(255, 255, 255)
+    grey_1 = (26, 26, 26) #rgb(26, 26, 26)
+    grey_2 = (35, 35, 35) #rgb(35, 35, 35)
+    grey_3 = (55, 55, 55) #rgb(55, 55, 55)
+
+    # Tetrimino colors
+    cyan = (69, 206, 204) #rgb(69, 206, 204) # I
+    blue = (64, 111, 249) #rgb(64, 111, 249) # J
+    orange = (253, 189, 53) #rgb(253, 189, 53) # L
+    yellow = (246, 227, 90) #rgb(246, 227, 90) # O
+    green = (98, 190, 68) #rgb(98, 190, 68) # S
+    pink = (242, 64, 235) #rgb(242, 64, 235) # T
+    red = (225, 13, 27) #rgb(225, 13, 27) # Z
+
+    t_color = [grey_2, cyan, blue, orange, yellow, green, pink, red, grey_3]
