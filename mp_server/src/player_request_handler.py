33abfaef2141f9ef63ed7a64132da909ef90bbe8
kshshkhim <kshshkim@gmail.com>
2021-10-31 17:44:51 +0900

    feature: request 스키마 작업중

--- mp_server/src/player_request_handler.py
+++ mp_server/src/player_request_handler.py
@@ -1,36 +1,35 @@
 from .multiplayer_manager import MultiplayerManager
 
+
 class PlayerConnection:
-    def __init__(self, user_id: str, websocket, mp_manager: MultiplayerManager):
+    def __init__(self, player_id: str, mp_manager: MultiplayerManager):
         self.status = 'hello'
-        self.user_id = user_id
+        self.player_id = player_id
         self.mpm = mp_manager
 
-    async def void_req(self, func, **kwargs):
-        await func(**kwargs)
-        return 'ok'
-
-    async def parse_request(self, data: dict, player_id: str):
+    async def parse_request(self, data: dict):
         req_type = data.get('type')
         waiter_id = data.get('waiter_id')
         solicitor_id = data.get('solicitor_id')
-
-        if req_type == 'add_to_waiting':
-            await self.void_req(self.mpm.add_to_waiting_list, player_id=player_id)
-            return 'ok'
-        if req_type == 'add_to_waiting':
-            await self.mpm.add_to_waiting_list(player_id=player_id)
-            return 'ok'
+        opponent_id = data.get('opponent')
+        game_data = data.get('game_data')
+        if req_type == 'game_data':
+            await self.mpm.send_game_data(game_data, opponent_id)
+        elif req_type == 'add_to_waiting':
+            self.status = 'waiting'
+            await self.mpm.add_to_waiting_list(player_id=self.player_id)
         elif req_type == 'quit_waiting':
-            await self.mpm.remove_from_waiting_list(player_id=player_id)
-            return 'ok'
+            self.status = 'hello'
+            await self.mpm.remove_from_waiting_list(player_id=self.player_id)
         elif req_type == 'solicit':
             if waiter_id is not None:
-                await self.mpm.solicit(player_id, waiter_id=waiter_id)
-                return 'ok'
+                self.status = 'soliciting'
+                await self.mpm.solicit(solicitor_id=self.player_id, waiter_id=waiter_id)
         elif req_type == 'get_solicitors':
-            solicitors = await self.mpm.get_solicitors(player_id)
+            solicitors = await self.mpm.get_solicitors(waiter_id=self.player_id)
             return solicitors
         elif req_type == 'accept':
-            await self.mpm.accept_match(solicitor_id=solicitor_id, waiter_id=self.user_id)
-            return 'ok'
+            self.status = 'match_accepted'
+            await self.mpm.accept_match(solicitor_id=solicitor_id, waiter_id=self.player_id)
+
+
