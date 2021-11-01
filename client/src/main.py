be37481b297be115c35ef141d33bc03c87bc5c02
kshshkhim <kshshkim@gmail.com>
2021-11-01 10:32:05 +0900

    fix: 멀티플레이 화면 출력 프로토타입

--- client/src/main.py
+++ client/src/main.py
@@ -14,15 +14,17 @@ class OTS:
         self.clock = pygame.time.Clock()  # 타이머 이벤트 발생기
         self.running = True  # 가동 상태, 현재 이벤트 핸들러에서 pygame.quit()을 이용해 끄게 되는데, 손을 봐야할듯함.
 
-        self.game_session = GameInstance(self.is_multiplayer)  # 게임 로직, 상태 등 처리
-        self.event_handler = EventHandler(self.game_session)  # 키 입력, 타이머 등 이벤트 처리
-        self.display_drawer = DisplayDrawer(self.game_session)  # 화면 업데이트 처리
-        self.game_instance = GameInstance(self)
-        self.pygame = pygame
+        self.game_instance = GameInstance(self.is_multiplayer)  # 게임 로직, 상태 등 처리
+
         if is_multiplayer:
-            self.websocket_client = OTSWebsocket('a1234', self.game_session)
+            self.multiplayer_instance = GameInstance()
+            self.websocket_client = OTSWebsocket('a1234', self.game_instance, self.multiplayer_instance)
             # self.websocket_client.opponent = '4567'
             self.wsc_thread = threading.Thread(target=self.websocket_client.run_forever, daemon=True)
+
+        self.event_handler = EventHandler(self.game_instance)  # 키 입력, 타이머 등 이벤트 처리
+        self.display_drawer = DisplayDrawer(self.game_instance, self.multiplayer_instance)  # 화면 업데이트 처리
+        self.pygame = pygame
 
         pygame.init()  # 파이게임 초기화
         pygame.time.set_timer(pygame.USEREVENT, 50)  # 0.05초마다 이벤트 생성
