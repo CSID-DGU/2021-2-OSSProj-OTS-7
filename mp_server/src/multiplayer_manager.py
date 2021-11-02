c7d4e0a8a43a163e6232c93f0c5cb9ba9c0f8a03
kshshkhim <kshshkim@gmail.com>
2021-11-02 17:54:20 +0900

    fix: parse message async 오류 수정

--- mp_server/src/multiplayer_manager.py
+++ mp_server/src/multiplayer_manager.py
@@ -1,3 +1,5 @@
+import time
+
 import redis.exceptions
 from rejson import Client, Path
 
@@ -26,16 +28,15 @@ class MultiplayerManager:
         self.port = redis_port
         self.session = Client(host=self.host, port=self.port, db=0, decode_responses=True)  # 게임 세션 데이터 저장
         self.waiting = Client(host=self.host, port=self.port, db=1, decode_responses=True)  # 게임 대기열
-        self.msg_broker = Client(host=self.host, port=self.port, db=3, decode_responses=False)  # 메시지 브로커
+        self.msg_broker = Client(host=self.host, port=self.port, db=3, decode_responses=True)  # 메시지 브로커
         self.redis_pup_sub = self.msg_broker.pubsub()  # 메시지 브로커 pub_sub
 
         self.initial_subscribe()  # 메시지 채널 구독
 
     # 레디스 메시지 채널 구독
     def initial_subscribe(self):
-        to_subscribe = []
+        to_subscribe = ['waiting']
         self.redis_pup_sub.subscribe(to_subscribe)
-        self.redis_pup_sub.unsubscribe('afsef')
 
     def is_waiter_exist(self, waiter_id: str) -> bool:
         if self.waiting.jsonget(waiter_id, Path.rootPath()) is None:
@@ -60,42 +61,48 @@ class MultiplayerManager:
         solicitors = await self.solicitor_get(waiter_id=waiter_id)
         if solicitor_id not in solicitors:
             self.waiting.jsonarrappend(solicitor_id)
+            self.msg_broker.publish(waiter_id, '{"todo": "su"}')
 
     async def match_id_set(self, player1: str, player2: str):
         self.session.jsonset('opponent', Path(f'.{player1}'), player2)
         self.session.jsonset('opponent', Path(f'.{player2}'), player2)
 
-    async def match_id_get(self, player_id: str):
+    async def player_match_id_get(self, player_id: str):
         return self.session.jsonget('opponent', Path(f'.{player_id}'))
 
-    async def match_id_clear(self, player_id: str):
+    async def player_match_id_clear(self, player_id: str):
         self.session.jsondel('opponent', Path(f'.{player_id}'))
 
-    async def match_accept(self, solicitor_id: str, waiter_id: str):
-        await self.waiting_list_remove(waiter_id)
-        obj = get_session_obj(solicitor_id, waiter_id)
-        match_id = await self.match_id_get(waiter_id)
-        self.session.jsonset('match', Path(f'.{match_id}'), obj)
-        # TODO pickle 안 먹힘
+    async def solicitor_accept(self, solicitor_id: str, waiter_id: str):  # 제안 수락
+        self.waiting.jsondel(solicitor_id)
+        self.waiting.jsondel(waiter_id)
+        await self.match_id_set(solicitor_id, waiter_id)
+        await self.game_session_set(match_id=waiter_id, player1=solicitor_id, player2=waiter_id)
+        self.msg_broker.publish(solicitor_id, '{"todo": "sa"}')
+        self.msg_broker.publish(waiter_id, '{"todo": "sa"}')
+        time.sleep(3)
+        self.msg_broker.publish(solicitor_id, '{"todo": "gs"}')
+        self.msg_broker.publish(waiter_id, '{"todo": "gs"}')
 
-    async def game_data_send(self, data, target: str):
-        # TODO pickle 안 먹힘
-        pass
-
-    async def session_set(self, match_id, player1, player2):
+    async def game_session_set(self, match_id, player1, player2):  # 게임 세션 생성
         data = {
             player1: {},
             player2: {},
         }
         self.session.jsonset(match_id, Path.rootPath(), data)
 
-    async def session_info_update(self, data, match_id, player_id):
+    async def game_session_data_set(self, data, match_id, player_id):  # 게임 데이터 클라이언트에게 받아서 보냄
         try:
             data_to_put = data.get('game_data')
             self.session.jsonset(match_id, Path(f'.{player_id}'), data_to_put)
         except redis.exceptions.ResponseError:
-            await self.session_set(match_id, 'a3456', 'a1234')  # 테스트용 예외처리, 실제 서비스에선 수정 필요.
+            await self.game_session_set(match_id, 'a3456', 'a1234')  # 테스트용 예외처리, 실제 서비스에선 수정 필요.
 
-    async def session_info_get(self, match_id):
-        return self.session.jsonget(match_id)
+    async def game_data_opponent_get(self, match_id, player_id):  # 상대방 게임 정보만 보냄
+        raw = self.session.jsonget(match_id)
 
+        raw.pop(player_id)  # 자신 데이터만 뺌
+        return raw
+
+    async def game_session_clear(self, match_id):
+        pass
--- mp_server/src/player_connection.py
+++ mp_server/src/player_connection.py
@@ -9,14 +9,19 @@ class PlayerConnection:
         self.mpm = mp_manager
         self.ws = websocket
 
+        self.current_match_id = None
+
+    # 이하 요청 수신
     async def parse_request(self, data: dict):
         req_type = data.get('type')
         waiter_id = data.get('waiter_id')
         solicitor_id = data.get('solicitor_id')
         opponent_id = data.get('opponent')
         game_data = data.get('game_data')
+
         if req_type == 'game_data':
-            await self.mpm.game_data_send(game_data, opponent_id)
+            if self.status == 'in_game':
+                await self.mpm.game_session_data_set(data=game_data, match_id=self.current_match_id, player_id=self.player_id)
         elif req_type == 'add_to_waiting':
             self.status = 'waiting'
             await self.mpm.waiting_list_add(player_id=self.player_id)
@@ -32,6 +37,25 @@ class PlayerConnection:
             return solicitors
         elif req_type == 'accept':
             self.status = 'match_accepted'
-            await self.mpm.match_accept(solicitor_id=solicitor_id, waiter_id=self.player_id)
+            await self.mpm.solicitor_accept(solicitor_id=solicitor_id, waiter_id=self.player_id)
 
+    # 이하 데이터 전송
+    async def send_game_info(self):
+        if self.current_match_id is None:
+            self.current_match_id = self.mpm.player_match_id_get(player_id=self.player_id)
+        await self.ws.send_json(await self.mpm.game_data_opponent_get(self.current_match_id, self.player_id))
 
+    async def send_event(self, event_code: str):
+        to_send = {
+            'type': event_code,
+        }
+        await self.ws.send_json(to_send)
+
+    async def send_error(self, error_msg):
+        to_send = {
+            'error': error_msg
+        }
+        await self.ws.send_json(to_send)
+
+    async def send_solicitors(self):
+        await self.mpm.solicitor_get(self.player_id)
