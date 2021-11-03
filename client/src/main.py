f14e38d26663a8cc2337f8d13c4d7a771dbfcafc
kshshkhim <kshshkim@gmail.com>
2021-11-03 17:55:48 +0900

    fix: 게임 모드 변수 생성 등

--- client/src/main.py
+++ client/src/main.py
@@ -8,24 +8,25 @@ from .ot_websocket import OTSWebsocket
 
 
 class OTS:
-    def __init__(self, is_multiplayer=True, player_id='a1234'):
-        self.is_multiplayer = is_multiplayer  # 멀티플레이어 여부
+    def __init__(self, game_mode: str = 'single', player_id: str = 'offline'):
+        self.game_mode = game_mode
+        self.game_instance = None
+        self.multiplayer_instance = None
+        if self.game_mode == 'online' or self.game_mode == 'dual':
+            self.player_id = player_id
+            self.multiplayer_instance = GameInstance()  # 게임 인스턴스 그대로 재활용, 성능상 문제 없으면 그대로 유지해도 무방할듯
+            self.game_instance = GameInstance(is_multiplayer=True)  # 게임 로직, 상태 등 처리
+        else:
+            self.game_instance = GameInstance()
+
+        if self.game_mode == 'online':
+            self.websocket_client = OTSWebsocket(self.player_id, self.game_instance, self.multiplayer_instance)
+            self.wsc_thread = threading.Thread(target=self.websocket_client.run_forever, daemon=True)  # 웹 소켓 연결 스레드
 
         self.clock = pygame.time.Clock()  # 타이머 이벤트 발생기
         self.running = True  # 가동 상태, 현재 이벤트 핸들러에서 pygame.quit()을 이용해 끄게 되는데, 손을 봐야할듯함.
 
-        self.game_instance = GameInstance(self.is_multiplayer)  # 게임 로직, 상태 등 처리
-
-        self.player_id = player_id
-
-        if is_multiplayer:
-            self.multiplayer_instance = GameInstance()  # 게임 인스턴스 그대로 재활용, 성능상 문제 없으면 그대로 유지해도 무방할듯
-            self.websocket_client = OTSWebsocket(self.player_id, self.game_instance, self.multiplayer_instance)
-            self.wsc_thread = threading.Thread(target=self.websocket_client.run_forever, daemon=True)  # 웹 소켓 연결 스레드
-            self.display_drawer = DisplayDrawer(self.game_instance, self.multiplayer_instance)  # 화면 업데이트 처리
-        else:
-            self.display_drawer = DisplayDrawer(self.game_instance)
-
+        self.display_drawer = DisplayDrawer(self.game_instance, self.multiplayer_instance)  # 화면 업데이트 처리
         self.event_handler = EventHandler(self.game_instance)  # 키 입력, 타이머 등 이벤트 처리
         self.pygame = pygame
 
@@ -35,7 +36,7 @@ class OTS:
 
     # run_client.py 에서 실행함.
     def run_game(self):
-        if self.is_multiplayer:
+        if self.game_mode == 'online':
             self.wsc_thread.start()  # 웹소켓 스레드 시작
             self.websocket_client.thread.start()  # 0.1초마다 게임 정보 보내는 스레드, 추후 변동사항 발생시에만 보내도록 수정 필요
         self.main_loop()
@@ -54,7 +55,7 @@ class OTS:
             # 게임 종료
             if self.event_handler.quit:
                 self.pygame.quit()
-                if self.is_multiplayer:
+                if self.game_mode == 'online':
                     self.websocket_client.ws.close()  # 소켓 해제
                 sys.exit()
 
