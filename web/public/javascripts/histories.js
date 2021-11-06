99ca1aaaeb3d28c4feb6c5a917893d54758dd03a
junyong <thwnsdyd@gmail.com>
2021-11-06 17:52:23 +0900

    add : view's js

---
+++ web/public/javascripts/histories.js
@@ -0,0 +1,33 @@
+window.onload = () => {
+
+  init();
+
+  function init() {
+    const inputName = document.getElementById('inputName');
+    const btnFind = document.getElementById('btnFind');
+
+    btnFind.addEventListener('click', () => {
+      fetch('/histories/findUser', {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          'name': inputName.value
+        })
+      })
+      .then(res => res.json())
+      .then(json => {
+        if (json.msg === "success finding") {
+          alert("success Finding");
+          window.location.href = '/users';
+        } else {
+          alert("failed Finding");
+        }
+      }).catch(err => {
+        console.log(err);
+      });
+    });
+  }
+}
