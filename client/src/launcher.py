1a70403cb00c6fc7033c5bf20c4c86b2dc71aa83
junyong <thwnsdyd@gmail.com>
2021-11-10 14:03:56 +0900

    fix : os 호환에러 & 라이선스 문제 해결 # tkinter --> pyqt5 pyqt5 --> pyside6

--- client/src/launcher.py
+++ client/src/launcher.py
@@ -1,41 +1,47 @@
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+from .variables.ui_variables import UI_VARIABLES as uv
 from .main import OTS
 import webbrowser
-from PyQt5.QtWidgets import *
 import sys
+
 
 class Launcher(QWidget):
     def __init__(self):
         self.app = QApplication(sys.argv)
-        super().__init__()
-        self.initialize()
-
+        QWidget.__init__(self)
         self.game_mode = None
         self.player_id = 'offline'
+        self.initialize()
 
     def initialize(self):
+
         self.setGeometry(300, 300, 400, 300)
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.setWindowTitle('OTS')
+        self.setWindowIcon(QIcon(QPixmap(uv.tetris_image)))
 
-        single_btn = QPushButton("Single play")
-        single_btn.clicked.connect(self.single_btn_clicked)
-        layout.addWidget(single_btn)
+        self.single_btn = QPushButton("Single play")
+        self.single_btn.clicked.connect(self.single_btn_clicked)
+        self.layout.addWidget(self.single_btn)
 
-        dual_btn = QPushButton("Dual play")
-        dual_btn.clicked.connect(self.dual_btn_clicked)
-        layout.addWidget(dual_btn)
+        self.dual_btn = QPushButton("Dual play")
+        self.dual_btn.clicked.connect(self.dual_btn_clicked)
+        self.layout.addWidget(self.dual_btn)
 
-        online_btn = QPushButton("Online play")
-        single_btn.clicked.connect(self.online_btn_clicked)
-        layout.addWidget(online_btn)
+        self.online_btn = QPushButton("Online play")
+        self.online_btn.clicked.connect(self.online_btn_clicked)
+        self.layout.addWidget(self.online_btn)
 
-        signup_btn = QPushButton("Sign up")
-        signup_btn.clicked.connect(self.signup_btn_clicked)
-        layout.addWidget(signup_btn)
+        self.signup_btn = QPushButton("Sign up")
+        self.signup_btn.clicked.connect(self.signup_btn_clicked)
+        self.layout.addWidget(self.signup_btn)
 
-        help_btn = QPushButton("Help")
-        help_btn.clicked.connect(self.help_btn_clicked)
-        layout.addWidget(help_btn)
+        self.help_btn = QPushButton("Help")
+        self.help_btn.clicked.connect(self.help_btn_clicked)
+        self.layout.addWidget(self.help_btn)
+        self.show()
 
     def single_btn_clicked(self):
         self.game_mode = 'single'
@@ -59,9 +65,7 @@ class Launcher(QWidget):
         webbrowser.open("https://github.com/CSID-DGU/2021-2-OSSProj-OTS-7/blob/main/client/assets/img/help.png?raw=true")
 
     def run_launcher(self):
-        screen = Launcher()
-        screen.show()
-        sys.exit(self.app.exec_())
+        self.app.exec_()
 
     def run_game(self):
         ots = OTS(game_mode=self.game_mode, player_id=self.player_id)
