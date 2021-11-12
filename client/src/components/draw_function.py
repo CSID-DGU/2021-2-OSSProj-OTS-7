286a72fe500f4da77f10134dcb436508bde02534
daryu519 <dbehddks519@naver.com>
2021-11-12 19:06:50 +0900

    add game_item interface

--- client/src/components/draw_function.py
+++ client/src/components/draw_function.py
@@ -1,3 +1,4 @@
+import pygame
 from pygame.draw import rect
 from ..variables.ui_variables import UI_VARIABLES
 from .mino import Mino
@@ -48,7 +49,7 @@ def draw_game_instance(game_instance, screen, x_mod=0):
     )
 
     # draw next_mino
-    draw_mino(220+x_mod, 140, game_instance.next_mino, 0, screen)
+    draw_mino(220+x_mod, 85, game_instance.next_mino, 0, screen)
 
     # draw hold_mino
     if game_instance.hold_mino is not None:
@@ -57,6 +58,25 @@ def draw_game_instance(game_instance, screen, x_mod=0):
     # Set max score
     if game_instance.score > 999999:
         score = 999999
+    # item image
+    bomb_image = pygame.image.load("assets/img/bomb.png")
+    bomb_image = pygame.transform.scale(bomb_image, (30, 30))
+    left_bomb = pygame.transform.scale(bomb_image, (15, 15))
+
+    clock_image = pygame.image.load("assets/img/clock.png")
+    clock_image = pygame.transform.scale(clock_image, (30, 30))
+    left_clock = pygame.transform.scale(clock_image, (15, 15))
+
+    # 현재 사용 가능한 item, Left item 보여줌
+    if game_instance.my_item_list:
+        if game_instance.my_item_list[0] == "bomb":
+            screen.blit(bomb_image, [x_mod+215, 335])
+        elif game_instance.my_item_list[0] == "clock":
+            screen.blit(clock_image, [x_mod + 215, 335])
+
+    screen.blit(left_bomb, [x_mod + 255, 335])
+    screen.blit(left_clock, [x_mod + 255, 355])
+
 
     # Draw texts
     # TODO 하드코딩 수정할것
@@ -68,16 +88,31 @@ def draw_game_instance(game_instance, screen, x_mod=0):
     level_value = UI_VARIABLES.h4.render(str(game_instance.level), 1, UI_VARIABLES.black)
     text_goal = UI_VARIABLES.h5.render("GOAL", 1, UI_VARIABLES.black)
     goal_value = UI_VARIABLES.h4.render(str(game_instance.goal), 1, UI_VARIABLES.black)
+    text_item = UI_VARIABLES.h5.render("ITEM    LEFT", 1, UI_VARIABLES.black)
+
+    bomb_count = 0
+    clock_count = 0
+    if game_instance.my_item_list:
+        bomb_count = game_instance.my_item_list.count('bomb')
+        clock_count = game_instance.my_item_list.count('clock')
+
+    left_item_bomb = UI_VARIABLES.h6.render("x " + str(bomb_count), 1, UI_VARIABLES.black)
+    left_item_clock = UI_VARIABLES.h6.render("x " + str(clock_count), 1, UI_VARIABLES.black)
+
 
     # Place texts
     screen.blit(text_hold, (x_mod+215, 14))
-    screen.blit(text_next, (x_mod+215, 104))
-    screen.blit(text_score, (x_mod+215, 194))
-    screen.blit(score_value, (x_mod+220, 210))
-    screen.blit(text_level, (x_mod+215, 254))
-    screen.blit(level_value, (x_mod+220, 270))
-    screen.blit(text_goal, (x_mod+215, 314))
-    screen.blit(goal_value, (x_mod+220, 330))
+    screen.blit(text_next, (x_mod+215, 54))
+    screen.blit(text_score, (x_mod+215, 134))
+    screen.blit(score_value, (x_mod+220, 154))
+    screen.blit(text_level, (x_mod+215, 194))
+    screen.blit(level_value, (x_mod+220, 214))
+    screen.blit(text_goal, (x_mod+215, 254))
+    screen.blit(goal_value, (x_mod+220, 274))
+    screen.blit(text_item, (x_mod + 215, 314))
+    screen.blit(left_item_bomb, (x_mod + 275, 335))
+    screen.blit(left_item_clock, (x_mod + 275, 355))
+
 
     # Draw board
     for x in range(UI_VARIABLES.init_board_width):
