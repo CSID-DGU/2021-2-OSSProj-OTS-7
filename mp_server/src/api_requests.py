5802ca759aa7c0e997e9791f49010db2d513a760
jjunyong-e <thwnsdyd@gmail.com>
2021-11-26 18:15:45 +0900

    fix : history post arguments add secret key

--- mp_server/src/api_requests.py
+++ mp_server/src/api_requests.py
@@ -1,10 +1,12 @@
 import requests
 from .config import DB_SERVER_URL
+from .secret_key import SECRET_KEY
+
 
 
 async def db_post_winner(user_id: str):
-    requests.post(url=DB_SERVER_URL, data={'name': user_id})
+    requests.post(url=DB_SERVER_URL, data={'name': user_id, 'key': SECRET_KEY})
 
 
 async def db_post_loser(user_id: str):
-    requests.post(url=DB_SERVER_URL, data={'name': user_id})
+    requests.post(url=DB_SERVER_URL, data={'name': user_id, 'key': SECRET_KEY})
