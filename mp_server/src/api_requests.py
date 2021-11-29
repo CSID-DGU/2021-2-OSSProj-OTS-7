21c83ff1ba7cb502e04ac26add3de41190b5b95c
kshshkhim <kshshkim@gmail.com>
2021-11-29 19:29:49 +0900

    fix: secret key 없으면 api 요청 무시

--- mp_server/src/api_requests.py
+++ mp_server/src/api_requests.py
@@ -1,12 +1,19 @@
 import requests
 from .config import DB_SERVER_URL
-from .secret_key import SECRET_KEY
+try:
+    from .secret_key import SECRET_KEY
 
+    async def db_post_winner(user_id: str):
+        requests.post(url=DB_SERVER_URL, data={'name': user_id, 'key': SECRET_KEY})
 
+    async def db_post_loser(user_id: str):
+        requests.post(url=DB_SERVER_URL, data={'name': user_id, 'key': SECRET_KEY})
 
-async def db_post_winner(user_id: str):
-    requests.post(url=DB_SERVER_URL, data={'name': user_id, 'key': SECRET_KEY})
+except ModuleNotFoundError:
+    async def db_post_winner(user_id: str):
+        print(f'module not found err \n winner {user_id=}')
+        pass
 
-
-async def db_post_loser(user_id: str):
-    requests.post(url=DB_SERVER_URL, data={'name': user_id, 'key': SECRET_KEY})
+    async def db_post_loser(user_id: str):
+        print(f'module not found err \n loser {user_id=}')
+        pass
