46d855d89eabbaea22cebd3ca14571b585db2f26
kshshkhim <kshshkim@gmail.com>
2021-12-07 14:12:29 +0900

    fix: request timeout 처리

--- mp_server/src/api_requests.py
+++ mp_server/src/api_requests.py
@@ -4,10 +4,16 @@ try:
     from .secret_key import SECRET_KEY
 
     async def db_post_winner(user_id: str):
-        requests.post(url=DB_SERVER_URL + '/winner', data={'name': user_id, 'key': SECRET_KEY})
+        try:
+            requests.post(url=DB_SERVER_URL + '/winner', data={'name': user_id, 'key': SECRET_KEY}, timeout=2)
+        except requests.exceptions.Timeout:
+            print('timeout')
 
     async def db_post_loser(user_id: str):
-        requests.post(url=DB_SERVER_URL + '/loser', data={'name': user_id, 'key': SECRET_KEY})
+        try:
+            requests.post(url=DB_SERVER_URL + '/loser', data={'name': user_id, 'key': SECRET_KEY}, timeout=2)
+        except requests.exceptions.Timeout:
+            print('timeout')
 
 except ModuleNotFoundError:
     async def db_post_winner(user_id: str):
