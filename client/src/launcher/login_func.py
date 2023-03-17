e7545f57e90e223949989914978858ad25444dfb
Seonghyeon Kim <kshshkim@users.noreply.github.com>
2023-03-17 16:25:03 +0900

    fix: valuation err on some cases

--- client/src/launcher/login_func.py
+++ client/src/launcher/login_func.py
@@ -1,9 +1,14 @@
 import requests
+
 from ..consts.urls import URLS
 
 
 def request_login(login_id: str, pw: str) -> dict:
     res = requests.post(url=URLS.login_url,
-                        data={'name': login_id,
+                        json={'name': login_id,
                               'password': pw})
+    if res.status_code != 200:
+        res = requests.post(url=URLS.login_url,
+                            data={'name': login_id,
+                                  'password': pw})
     return res.json()
